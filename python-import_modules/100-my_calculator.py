ef806cb997c4d87ce26598c0125d4456c0fa0f88
N-Ghi <n.iyankijij@alustudent.com>
2023-11-02 07:03:27 -0700

    ok

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,22 +2,23 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-a, operator, b = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
+    a, operator, b = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
 
-if operator == "+":
-    result = add(a, b)
-elif operator == "-":
-    result = sub(a, b)
-elif operator == "*":
-    result = mul(a, b)
-elif operator == "/":
-    result = div(a, b)
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = sub(a, b)
+    elif operator == "*":
+        result = mul(a, b)
+    elif operator == "/":
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-print("{} {} {} = {}".format(a, operator, b, result))
+    print("{} {} {} = {}".format(a, operator, b, result))

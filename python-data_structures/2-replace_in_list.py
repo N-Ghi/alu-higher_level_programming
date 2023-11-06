70035e24c214d3614c202c85f7f0010014fc0395
N-Ghi <n.iyankijij@alustudent.com>
2023-11-06 00:20:11 -0800

    ok

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    for item in my_list:
+        if idx < 0 or idx >= len(my_list):
+            return my_list
+        else:
+            new_list = my_list.add(element, idx)

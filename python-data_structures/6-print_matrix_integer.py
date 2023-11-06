f9019e70ee3d5ab4b0cea542eb869acba8711ba3
N-Ghi <n.iyankijij@alustudent.com>
2023-11-06 02:23:42 -0800

    ok

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -9,6 +9,6 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d} ".format(num), end="")
             else:
                 print("{:d}".format(num))
-    if is not matrix:
+    if not matrix:
         print(" ")
         return

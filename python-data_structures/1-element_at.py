bd126b0c8015abf1f2e7cc5861a918bc469f1cf5
N-Ghi <n.iyankijij@alustudent.com>
2023-11-06 00:09:06 -0800

    ok

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for item in my_list:
+        if idx < 0 or idx > int(len(my_list)):
+            return None
+        else:
+            x = my_list[idex]
+            return x

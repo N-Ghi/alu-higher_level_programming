1284ff4819e70f1bc854400e1eb09d4d3751208b
N-Ghi <n.iyankijij@alustudent.com>
2023-11-06 04:12:56 -0800

    ok

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max_val = my_list[0]
+    for item in my_list:
+        if item > max_val:
+            max_val = item
+            return max_val

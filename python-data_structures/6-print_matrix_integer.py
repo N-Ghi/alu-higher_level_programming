4d4f63981acaf90e06c685a731a00fb767c50f34
N-Ghi <n.iyankijij@alustudent.com>
2023-11-06 01:57:45 -0800

    ok

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return
     for row in matrix:
         for i, num in enumerate(row):
             if i < len(row) - 1:

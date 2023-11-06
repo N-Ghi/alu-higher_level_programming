a9c0a639bebd25212d148e4f0fe04c3abc42dbdb
N-Ghi <n.iyankijij@alustudent.com>
2023-11-05 23:49:23 -0800

    ok

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for item in my_list:
-        print(item)
-
-
-my_lists = input("Enter the list: ")
-print_list_integer(my_lists)
+        print("{:d}".format(item))

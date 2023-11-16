9eb03dce4d6a2cde3c64f2fa2a33c08271010faf
N-Ghi <n.iyankijij@alustudent.com>
2023-11-16 02:19:08 -0800

    ok

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Defines a locked class.
+"""Defines a locked class."""
 
-"""
+
 class LockedClass:
 """Doesn't allow the creation of other attributes"""
+
 
     __slots__ = ["first_name"]

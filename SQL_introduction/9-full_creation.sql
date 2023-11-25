09cbab339824cbcbef4c4240ed8b1455ce6a41b0
N-Ghi <n.iyankijij@alustudent.com>
2023-11-25 01:37:23 -0800

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- Creating a second table
-CREATE TABLE IF NOT EXISTS second_table(
-id INT
-name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS second_table (
+id INT,
+name VARCHAR(256),
 score INT
 );
 INSERT INTO second_table(id, name, score)

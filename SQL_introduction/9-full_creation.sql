0afc6ae8e864ae07915408291d0635eb4d6c460d
N-Ghi <n.iyankijij@alustudent.com>
2023-11-25 01:28:44 -0800

    9-full_creation.sql

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- Creating a second table
+CREATE TABLE second_table(
+id INT
+name VARCHAR(256)
+score INT);
+INSERT INTO seond_table VALUES(1, "John", 10)
+INSERT INTO seond_table VALUES(2, "Alex", 3)
+INSERT INTO seond_table VALUES(3, "Bob", 14)
+INSERT INTO seond_table VALUES(4, "George", 8)

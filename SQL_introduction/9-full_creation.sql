2d15020ac1ef4dcd06b051ae6c2fb16df56d7fe3
N-Ghi <n.iyankijij@alustudent.com>
2023-11-25 01:35:36 -0800

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,7 +4,8 @@ id INT
 name VARCHAR(256)
 score INT
 );
-INSERT INTO seond_table VALUES(1, "John", 10);
-INSERT INTO seond_table VALUES(2, "Alex", 3);
-INSERT INTO seond_table VALUES(3, "Bob", 14);
-INSERT INTO seond_table VALUES(4, "George", 8);
+INSERT INTO second_table(id, name, score)
+VALUES  (1, 'John', 10),
+        (2, 'Alex', 3),
+        (3, 'Bob', 14),
+        (4, 'George', 8);

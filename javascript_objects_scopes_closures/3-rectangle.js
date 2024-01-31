5a4a41a036fdb766867f7f8238ce48f0b908eb74
Ghyghi <nagasaroghislaine3@gmail.com>
2024-01-31 18:13:05 +0200

    okk?

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 const Rectangle = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
-    let prints = "";
+  print () {
+    let prints = '';
     for (let xx = 0; xx < this.height; xx++) {
       for (let yy = 0; yy < this.width; yy++) {
-        prints = prints + "X";
+        prints = prints + 'X';
       }
       console.log(prints);
-      prints = "";
+      prints = '';
     }
   }
 };
--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,30 +1,30 @@
 #!/usr/bin/node
 const Rectangle = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     let prints = "";
     for (let xx = 0; xx < this.height; xx++) {
       for (let yy = 0; yy < this.width; yy++) {
-        prints = prints + "X";
+        prints = prints + 'X';
       }
       console.log(prints);
-      prints = "";
+      prints = '';
     }
   }
 
-  rotate() {
+  rotate () {
     const p = this.width;
     this.width = this.height;
     this.height = p;
   }
 
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }

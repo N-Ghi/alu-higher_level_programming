62fb15d9c1956577e924ffd93dd01a7642dc3526
Ghyghi <nagasaroghislaine3@gmail.com>
2024-01-31 18:22:38 +0200

    okk

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2);
-const fs = require("fs");
+const fs = require('fs');
 const fileA = fs.readFileSync(args[0]);
 const fileB = fs.readFileSync(args[1]);
 const fileC = `${fileA}${fileB}`;

a27163b6022a912e9aaada3ba918318d6edbcb13
Ghyghi <nagasaroghislaine3@gmail.com>
2024-01-31 18:37:22 +0200

    okk?

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,9 +3,9 @@ class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
       return null;
-      this.width = w;
-      this.height = h;
     }
+    this.width = w;
+    this.height = h;
   }
 }
 

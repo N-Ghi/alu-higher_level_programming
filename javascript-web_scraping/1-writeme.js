12fe7693c04d63c7664798e74856e8cae6268947
Ghyghi <nagasaroghislaine3@gmail.com>
2024-02-06 11:28:31 +0200

    2

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,8 +2,8 @@
 
 const fs = require('fs');
 
-function writeFile(filePath, content) {
-  fs.writeFile(filePath, content, "utf-8", (err) => {
+function writeFile (filePath, content) {
+  fs.writeFile(filePath, content, 'utf-8', (err) => {
     if (err) {
       console.error(`An error occurred while writing to the file: ${err}`);
     }
@@ -15,5 +15,5 @@ if (process.argv.length > 3) {
   const content = process.argv[3];
   writeFile(filePath, content);
 } else {
-  console.log("provide the file path and content as arguments.");
+  console.log('provide the file path and content as arguments.');
 }
